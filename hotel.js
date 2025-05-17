690642e51e5614f39228acae5a55a98bf184d2fe
silvz67 <silvaartr661@gmail.com>
2025-05-17 11:08:06 -0300

    Primeiro commit Hotel stars

---
+++ hotel.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const instaIcon = document.querySelector(".insta");
+    const whatsIcon = document.querySelector(".whats");
+  
+    if (instaIcon) {
+      instaIcon.addEventListener("click", function () {
+        window.open("https://www.instagram.com/_Artur.082", "_blank"); 
+      });
+    }
+  
+    if (whatsIcon) {
+      whatsIcon.addEventListener("click", function () {
+        window.open("https://wa.me/5582996117038", "_blank"); 
+      });
+    }
+  });
+  
